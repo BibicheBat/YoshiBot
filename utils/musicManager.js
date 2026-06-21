d71d2501a1405a04fe987adb4bc939c89d366de0
BibicheBat <bibicheleroi@icloud.com>
2026-06-21 20:41:56 +0200

    Fix YoutubeExtractor

--- utils/musicManager.js
+++ utils/musicManager.js
@@ -1,5 +1,6 @@
 // utils/musicManager.js
 const { Player } = require('discord-player');
+const { YoutubeExtractor } = require('@discord-player/extractor');
 
 let player;
 
@@ -8,31 +9,11 @@ async function getPlayer(client) {
 
   player = new Player(client);
 
-  // Debug : voir ce qu'exporte @discord-player/extractor
   try {
-    const extractor = require('@discord-player/extractor');
-    console.log('[Music] Exports disponibles:', Object.keys(extractor));
-
-    // Essaie tous les extractors possibles
-    const toTry = [
-      'YoutubeiExtractor',
-      'YouTubeExtractor', 
-      'SoundCloudExtractor',
-      'SpotifyExtractor',
-    ];
-
-    for (const name of toTry) {
-      if (extractor[name]) {
-        try {
-          await player.extractors.register(extractor[name], {});
-          console.log(`[Music] ${name} chargé !`);
-        } catch(e) {
-          console.error(`[Music] ${name} erreur:`, e.message);
-        }
-      }
-    }
+    await player.extractors.register(YoutubeExtractor, {});
+    console.log('[Music] YoutubeExtractor chargé !');
   } catch(e) {
-    console.error('[Music] Erreur import extractor:', e.message);
+    console.error('[Music] Erreur YoutubeExtractor:', e.message);
   }
 
   console.log('[Music] Extractors actifs:', [...player.extractors.store.keys()]);
