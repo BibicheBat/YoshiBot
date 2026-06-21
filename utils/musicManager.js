3163943535f14bcfa388eeb8d0a9f696959445a2
BibicheBat <bibicheleroi@icloud.com>
2026-06-21 20:35:07 +0200

    Fix versions discord-player 6.6.6

--- utils/musicManager.js
+++ utils/musicManager.js
@@ -1,16 +1,28 @@
 // utils/musicManager.js
 const { Player } = require('discord-player');
-const { YoutubeiExtractor } = require('@discord-player/extractor');
 
 let player;
 
 async function getPlayer(client) {
   if (player) return player;
+
   player = new Player(client);
-  await player.extractors.register(YoutubeiExtractor, {
-    streamOptions: { useClient: 'ANDROID' }
-  });
-  console.log('[Music] discord-player prêt');
+
+  try {
+    const { YoutubeiExtractor } = require('@discord-player/extractor');
+    await player.extractors.register(YoutubeiExtractor, {});
+    console.log('[Music] YoutubeiExtractor OK');
+  } catch(e1) {
+    console.error('[Music] YoutubeiExtractor échoué:', e1.message);
+    try {
+      await player.extractors.loadDefault();
+      console.log('[Music] Extractors par défaut chargés');
+    } catch(e2) {
+      console.error('[Music] Impossible de charger les extractors:', e2.message);
+    }
+  }
+
+  console.log('[Music] Extractors actifs:', [...player.extractors.store.keys()]);
   return player;
 }
 
